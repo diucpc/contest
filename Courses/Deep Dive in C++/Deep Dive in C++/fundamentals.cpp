8f6266983efb947dcfa23030622f6f126fb0e4f2
lifeoflikhon <mehedi.hasansjs@gmail.com>
2020-02-22 04:28:30 +0600

    added cpp

---
+++ Courses/Deep Dive in C++/Deep Dive in C++/fundamentals.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string username; // variables
+    getline(cin, username); // getting value
+    cout << "Hey, " << username << endl; // printing something
+    return 0;
+}
